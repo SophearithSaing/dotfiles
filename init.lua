ac8ffe26a02981bf1b33afd016bbca04f99303a5
SophearithSaing <sophearithsaing123@gmail.com>
2026-06-03 15:03:16 +0700

    Add lualine, telescope, color and lsp errors/warnings

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+vim.g.mapleader = " "
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -85,4 +87,67 @@ require("lazy").setup({
 		  })
 	  end,
   },
+
+  -- Statusline: shows git branch
+  {
+	  "nvim-lualine/lualine.nvim",
+	  dependencies = {
+		  "nvim-tree/nvim-web-devicons",
+	  },
+	  config = function()
+		  require("lualine").setup({
+			  options = {
+				  theme = "auto",
+				  globalstatus = true,
+			  },
+			  sections = {
+				  lualine_b = { "branch", "diff", "diagnostics" },
+			  },
+		  })
+	  end,
+  },
+
+  -- Quick search / fuzzy finder
+  {
+	  "nvim-telescope/telescope.nvim",
+	  dependencies = {
+		  "nvim-lua/plenary.nvim",
+	  },
+	  config = function()
+		  local builtin = require("telescope.builtin")
+
+		  vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
+		  vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Search text" })
+		  vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
+		  vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find help" })
+	  end,
+  }, 
+
+  -- Theme
+  {
+	  "Mofiqul/vscode.nvim",
+	  priority = 1000,
+	  config = function()
+		  require("vscode").setup({
+			  transparent = true,
+		  })
+
+		  vim.cmd.colorscheme("vscode")
+	  end,
+  },
 })
+
+-- Transparent background
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+-- Diagnostics: show LSP errors/warnings and navigate between them
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show line error" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "File diagnostics" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
